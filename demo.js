2913cd0e9b58ee52c4f1b382ced237444783f9f0
Daniel Hug <danielthehug@gmail.com>
2014-12-17 00:05:17 -0800

    add action option

--- demo.js
+++ demo.js
@@ -1,11 +1,16 @@
-var cols = document.querySelectorAll('.cols .dl-item');
-var dl = new DragList({
-	itemEls: cols,
+var colsEl = document.querySelector('.cols');
+var colsDragList = new DragList({
+	itemEls: colsEl.children,
 	handleSelector: 'header',
 	callback: function(srcEl) {
 		// Set number of times the column has been moved.
 		var newCount = parseInt(srcEl.getAttribute('data-col-moves')) + 1;
 		srcEl.setAttribute('data-col-moves', newCount);
 		srcEl.lastChild.data = 'moves: ' + newCount;
-	}
+	},
+	action: 'switch'
+});
+
+var groceriesDragList = new DragList({
+	itemEls: document.querySelectorAll('.groceries li')
 });
--- draglist.js
+++ draglist.js
@@ -7,6 +7,17 @@ window.DragList = (function() {
 
 	// helpers
 
+	function arrayRemove(array, item) {
+		var i = array.indexOf(item);
+		if (i > -1) array.splice(i, 1);
+	}
+
+	function arrayExcept(collection, item) {
+		var copy = [].slice.call(collection);
+		arrayRemove(copy, item);
+		return copy;
+	}
+
 	function swapNodes(a, b) {
 		var aParent = a.parentNode;
 		var aSibling = a.nextSibling === b ? a : a.nextSibling;
@@ -25,7 +36,9 @@ window.DragList = (function() {
 	var DragList = function(options) {
 		// attach option data to this
 		this.handleSelector = options.handleSelector || 'dl-handle';
+		this.action = options.action || 'move';
 		this.callback = options.callback || null;
+		this.dropAreaEl = null;
 
 		// make each element draggable, add them to this.itemEls, and set up events
 		this.itemEls = [];
@@ -35,9 +48,34 @@ window.DragList = (function() {
 	DragList.prototype.addItem = function(itemEl) {
 		this.itemEls.push(itemEl);
 		itemEl.draggable = true;
- 
+		itemEl.classList.add('dl-item');
+
 		// cache this DragList instance for use in handlers
 		var thisDragList = this;
+
+
+		// create dropAreaEl if it doesn't already exist
+		if (this.action === 'move' && !this.dropAreaEl) {
+			this.dropAreaEl = document.createElement(itemEl.tagName);
+			this.dropAreaEl.className = 'dl-drop-area';
+
+
+			// dropAreaEl handlers
+
+			on(this.dropAreaEl, 'dragover', function(e) {
+				e.preventDefault(); // required for drop
+			});
+
+			on(this.dropAreaEl, 'drop', function(e) {
+				if (e.stopPropagation) {
+					e.stopPropagation(); // stops the browser from redirecting.
+				}
+
+				// callback
+				if (thisDragList.callback)
+					thisDragList.callback(thisDragList.curSrcEl);
+			});
+		}
 
 
 		// itemEl handlers
@@ -58,37 +96,59 @@ window.DragList = (function() {
 
 			e.dataTransfer.effectAllowed = 'move';
 			thisDragList.curSrcEl = this;
-			this.classList.add(MOVING_CLASS);
+			if (thisDragList.action === 'switch')
+				this.classList.add(MOVING_CLASS);
 		});
 
-		on(itemEl, 'dragover', function(e) {
+		on(itemEl, 'dragover', thisDragList.action === 'switch' ? function(e) {
 			e.preventDefault();
 			e.dataTransfer.dropEffect = 'move';
 			this.classList.add(OVER_CLASS);
-		});
+		} : function(e) {
+			var dropAreaEl = thisDragList.dropAreaEl;
+			var parent = this.parentNode;
 
-		// this also fires when a child node is dragged over
-		on(itemEl, 'dragleave', function(e) {
-			this.classList.remove(OVER_CLASS);
-		});
-
-		on(itemEl, 'drop', function(e) {
-			if (e.stopPropagation) {
-				e.stopPropagation(); // stops the browser from redirecting.
-			}
-
-			// Don't do anything if we're dropping on the same item we're dragging.
-			if (thisDragList.curSrcEl !== this) {
-				swapNodes(thisDragList.curSrcEl, this);
-
-				// callback
-				if (thisDragList.callback)
-					thisDragList.callback(thisDragList.curSrcEl, this);
+			if (this === thisDragList.curSrcEl) {
+				// replace element being dragged with dropAreaEl
+				parent.replaceChild(dropAreaEl, this);
+			} else {
+				var targetI = [].indexOf.call(parent.children, this);
+				var visibleItemEls = arrayExcept(parent.children, dropAreaEl);
+				// move dropAreaEl
+				parent.insertBefore(dropAreaEl, visibleItemEls[targetI]);
 			}
 		});
+
+		if (this.action === 'switch') {
+			// this also fires when a child node is dragged over
+			on(itemEl, 'dragleave', function(e) {
+				this.classList.remove(OVER_CLASS);
+			});
+
+			on(itemEl, 'drop', function(e) {
+				if (e.stopPropagation) {
+					e.stopPropagation(); // stops the browser from redirecting.
+				}
+
+				// Don't do anything if we're dropping on the same item we're dragging.
+				if (thisDragList.curSrcEl !== this) {
+					swapNodes(thisDragList.curSrcEl, this);
+
+					// callback
+					if (thisDragList.callback)
+						thisDragList.callback(thisDragList.curSrcEl, this);
+				}
+			});
+		}
 
 		on(itemEl, 'dragend', function() {
-			this.classList.remove(MOVING_CLASS);
+			if (thisDragList.action === 'switch') {
+				this.classList.remove(MOVING_CLASS);
+			} else {
+				// replace dropAreaEl with srcEl
+				var dropAreaEl = thisDragList.dropAreaEl;
+				dropAreaEl.parentNode.replaceChild(this, dropAreaEl);
+			}
 
 			// remove hover styles from every item
 			[].forEach.call(thisDragList.itemEls, function(itemEl) {
